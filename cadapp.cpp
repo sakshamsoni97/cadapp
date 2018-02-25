e62dd4b5f7c59917e2eeb7857d75fbcdbd99fc41
Saksham Soni <saksham9315soni@gmail.com>
2018-02-25 15:47:12 +0530

    more comments

--- cadapp.cpp
+++ cadapp.cpp
@@ -1,3 +1,8 @@
+/*! \file cadapp.cpp
+	\brief Frontend of the application
+
+	Front end and the UI of the application is handled here. UI is designed using GTK+/Qt library.
+*/
 #include <stdlib.h>
 #include <GL/glew.h>
 #include <GL/glfw3.h>
---
+++ drawing.cpp
@@ -0,0 +1,5 @@
+/*! \file drawing.cpp
+	\brief Implementation of the algorithms
+
+	This file contains the implementation of all the required algorithms for the CAD application.
+*/
