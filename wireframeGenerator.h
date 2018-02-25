3261f5562a7641c4693b5665c358bbd6de50fa04
IshanTewari <ishan.tewari96@gmail.com>
2018-02-25 17:34:41 +0530

    Added Files

---
+++ wireframeGenerator.h
@@ -0,0 +1,17 @@
+#include "drawing.h"
+
+using namespace::std
+
+class wireframeGenerator {
+  private:
+          static tree decision_tree;
+          list <edge> p_edge;
+          list <vertex> p_vertex;
+  public: void renderWireframe();
+          void wireframeGenerator()
+
+}
+
+class tree {
+
+}
