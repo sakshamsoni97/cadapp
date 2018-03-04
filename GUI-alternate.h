e66b6fafd49a5c455c3436fdbe6210d3c63a3ff2
IshanTewari <ishan.tewari96@gmail.com>
2018-03-04 19:10:56 +0530

    Documentation Update

--- GUI-alternate.h
+++ GUI-alternate.h
@@ -10,42 +10,78 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
+//! Class for the home screen UI
+/*!
+Helps the user to select the kind of conversion to be performed (2D-3D or 3D-2D)
+*/
 class HomeScreen {
   private:
   public:
+      /*! method to display the UI elements composing the home screen */
       ScreenPainter();
+      /*! method to invoke correct behavior depending on event/action invoked by user */
       EventHandler();
+      /*! method to improve robustness and handle errors/incorrect calls with/or console output */
       ErrorHandler();
 }
 
+//! Class for the for 2D to 3D conversion input screen UI
+/*!
+Helps the user to interact and specify the input specification of orthographic views to convert to a 3D object
+*/
 class inputScreen2d3d {
   private:
   public:
+      /*! method to display the UI elements composing the input screen */
       ScreenPainter();
+      /*! method to invoke correct behavior depending on event/action invoked by user */
       EventHandler();
+      /*! method to improve robustness and handle errors/incorrect calls with/or console output */
       ErrorHandler();
 }
 
+//! Class for the for 3D to 2D conversion input screen UI
+/*!
+Helps the user to interact and specify the input specification of a 3D object and prepare its orthographic projection
+*/
 class inputScreen3d2d {
   private:
   public:
+      /*! method to display the UI elements composing the input screen */
       ScreenPainter();
+      /*! method to invoke correct behavior depending on event/action invoked by user */
       EventHandler();
+      /*! method to improve robustness and handle errors/incorrect calls with/or console output */
       ErrorHandler();
 }
 
+
+//! Class for the for 2D to 3D conversion output screen UI
+/*!
+Displays the corresponding 3D object for the orthographic views specified in the input screen
+*/
 class outputScreen2d3d {
   private:
   public:
-      ScreenPainter();
+      /*! method to display the UI elements composing the output screen */
+      ScreenPainter
+      /*! method to invoke correct behavior depending on event/action invoked by user */
       EventHandler();
+      /*! method to improve robustness and handle errors/incorrect calls with/or console output */
       ErrorHandler();
 }
 
+//! Class for the for 3D to 2D conversion output screen UI
+/*!
+Displays the corresponding orthographic projections for the 3D object specified in the input screen
+*/
 class outputScreen3d2d {
   private:
   public:
+      /*! method to display the UI elements composing the output screen */
       ScreenPainter();
+      /*! method to invoke correct behavior depending on event/action invoked by user */
       EventHandler();
+      /*! method to improve robustness and handle errors/incorrect calls with/or console output */
       ErrorHandler();
 }
